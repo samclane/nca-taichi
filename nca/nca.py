377677f09849a42b2565c74bda3c2fc81d2160b1
Sawyer McLane <samclane@gmail.com>
2022-11-27 06:19:46 -0700

    Added functions for update step

--- nca/nca.py
+++ nca/nca.py
@@ -23,20 +23,57 @@ def sobel_y_filter(img: ti.template()):
             result += img[I - 1 + offset][:3] * sobel_y_filter_weights[offset]
         img[I][7:10] = result
 
-
-
 def percieve(state_grid: ti.template()):
     sobel_y_filter(state_grid)
     sobel_x_filter(state_grid)
+
+
+def dense(input_vector: ti.template(), output_length: ti.i32):
+    # convert input_vector to a 1D vector
+    for i in range(output_length):
+        result = ti.Vector.zero(ti.f32, 1)
+        for j in range(input_vector.n):
+            result += input_vector[j] * ti.random()
+        input_vector[i] = result
+    return input_vector
+
+
+# simulate a relu activation
+def relu(input_vector: ti.template()):
+    for i in range(input_vector.n):
+        if input_vector[i] < 0:
+            input_vector[i] = 0
+    return input_vector
+
+
+def update_cell(cell: ti.template()):
+    # simulate a dense neural network
+    x = dense(cell, 128)
+    x = relu(x)
+    x = dense(x, 16)
+    return x
+
+
+@ti.kernel
+def update(state_grid: ti.template()):
+    for I in ti.grouped(state_grid):
+        # convert to a vector
+        state_grid[I] = update_cell(state_grid[I])
+
 
 # read image into state_grid
 img = ti.tools.imread('./res/example.jpg')
 state_grid = ti.Vector.field(n=16, dtype=ti.f32, shape=(img.shape[0], img.shape[1]))
 state_grid.from_numpy(img)
-print(img[0,0])
 percieve(state_grid)
 # display img channel first
 ti.tools.imshow(state_grid.to_numpy()[:, :, :4])
 ti.tools.imshow(state_grid.to_numpy()[:, :, 4:7])
 ti.tools.imshow(state_grid.to_numpy()[:, :, 7:10])
 
+update(state_grid)
+
+# display img channel first
+ti.tools.imshow(state_grid.to_numpy()[:, :, :4])
+ti.tools.imshow(state_grid.to_numpy()[:, :, 4:7])
+ti.tools.imshow(state_grid.to_numpy()[:, :, 7:10])
